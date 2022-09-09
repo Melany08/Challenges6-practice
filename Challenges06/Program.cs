aa20f76c39a4fb6b300e00d8c7d0687840038bb8
Melany Lopez <Melanysoloso@gmail.com>
2022-09-09 09:21:15 -0600

    message

---
+++ Challenges06/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+ushort score =5;
+Console.WriteLine($"You scored {score} points (ushort).");
+float otherscore;
+otherscore = 1.2F;
+Console.WriteLine($"Your other score is {otherscore} (float)");
+
+//...do tat for all data types
+
+//challenge 2 - modify the values
+score = 10;
